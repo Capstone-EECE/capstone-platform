ab4fe5d44413469fe6c90dce31eaa46705a2d3b9
diegovaldiviac <valdiviacox.d@northeastern.edu>
2023-11-09 00:59:39 -0500

    succesfull custom event listen and send

--- server/api/socket_events.py
+++ server/api/socket_events.py
@@ -1,23 +1,56 @@
+import socket
+
 from flask_socketio import emit
 
 from ..core.drone_location import get_drone_gps_coordinates
 from .app import socketio
 
+BG96_DEVICE_PORT = 1234
+BG96_DEVICE_IP = "TEMP"
 
-@socketio.on("connect")
+
+# @socketio.on("connect")
 def handle_connect():
     print("Client connected")
 
 
-# Assuming you have a function to get and emit drone GPS coordinates
-def emit_drone_gps_coordinates():
-    while True:
-        coordinates = get_drone_gps_coordinates()
-        emit("gpsUpdate", coordinates, namespace="/drone")
-        socketio.sleep(1)
+# @socketio.on("hello")
+def handle_hello(arg):
+    try:
+        print(arg)  # "world"
+        emit("response", "got it", broadcast=True)
+    except Exception as e:
+        print("Error: ", str(e))
+        emit("response", "Error occurred", broadcast=True)
 
 
-@socketio.on("disconnect", namespace="/drone")
+# @socketio.on('connect_drone')
+def handle_custom_event():
+    print("Received custom_event")
+
+    try:
+        # Create a socket connection to the BG96 modem
+        bg96_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        bg96_socket.connect(("temp", BG96_DEVICE_PORT))
+
+        # Send a message to the BG96 modem
+        bg96_socket.send(b"Your Message\r\n")
+
+        # Receive a response from the BG96 modem
+        response = bg96_socket.recv(1024)
+
+        # Close the socket connection
+        bg96_socket.close()
+
+        # Emit the response to the client
+        emit("response", response.decode("utf-8"), broadcast=True)
+
+    except Exception as e:
+        print("Error: ", str(e))
+        emit("response", "Error occurred", broadcast=True)
+
+
+# @socketio.on("disconnect")
 def handle_drone_disconnect():
     print("Client disconnected from drone namespace")
 
