0bef3ae0316e7ac0769537bac9635e9e13366b85
diegovaldiviac <valdiviacox.d@northeastern.edu>
2023-11-28 18:25:18 -0500

    socket connection moved to socket event file

--- server/api/app.py
+++ server/api/app.py
@@ -1,56 +1,17 @@
-# import apig_wsgi
-import logging
-import socket
-import time
-from http.client import HTTPException
-
-from flask import Flask, jsonify, request
+# app.py
+from flask import Flask
 from flask_cors import CORS
-from flask_socketio import SocketIO, emit
-from server.api.modem_routes import modem_endpoints
 from server.api.frontend_routes import frontend_endpoints
-from server.core.constants import REMOTE_IP, REMOTE_PORT
+from server.api.modem_routes import modem_endpoints
+from server.api.socket_events import socketio
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'secret!'
 CORS(app, resources={r"/*": {"origins": "*"}})
+# app.config['SECRET_KEY'] = 'your_secret_key'
 
-socketio = SocketIO(app, cors_allowed_origins="*", logger=True)
 app.register_blueprint(modem_endpoints, url_prefix="/modem")
 app.register_blueprint(frontend_endpoints, url_prefix="/frontend")
 
-
-@socketio.on("connect_drone")
-def handle_custom_event():
-    print("[REQUEST] turn on drone GPS coordinates")
-
-    try:
-        bg96_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print("Before socket connect")
-        time.sleep(1)
-        bg96_socket.connect((REMOTE_IP, REMOTE_PORT))
-        print("After socket connect")
-
-        # Send a message to the BG96 modem
-        bg96_socket.send(b"TURN ON GPS COORDINATES\r\n")
-
-        # Receive a response from the BG96 modem
-        response = bg96_socket.recv(1024)
-
-        # Close the socket connection
-        # TODO: We probably want to keep this socket open for continuing broadcast
-        # bg96_socket.close()
-
-        # Emit the response to the client
-        emit("GPS", response.decode("utf-8"), broadcast=True)
-
-    except Exception as e:
-        print("Error: ", str(e))
-        emit("GPS", "Error occurred connecting to drone", broadcast=True)
-
-
 if __name__ == "__main__":
+    socketio.init_app(app)
     socketio.run(app, host="0.0.0.0", debug=True, port=5235)
-
-
-# lambda_handler = apig_wsgi.make_lambda_handler(app)
--- server/api/socket_events.py
+++ server/api/socket_events.py
@@ -1,28 +1,12 @@
-import socket
-
 from flask import request
-from flask_socketio import emit
+from flask_socketio import SocketIO, emit
 
-from ..core.drone_location import get_drone_gps_coordinates
-from .app import socketio
-
-BG96_DEVICE_PORT = 1234
-BG96_DEVICE_IP = "TEMP"
-
-bg96_socket = None
-
-
-def connect_to_hardware():
-    if bg96_socket == None:
-        # Create a socket connection to the BG96 modem
-        bg96_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        bg96_socket.connect(("temp", BG96_DEVICE_PORT))
+socketio = SocketIO(cors_allowed_origins="*", logger=True)
 
 
 @socketio.on("connect")
 def handle_connect():
-    print(request.sid)
-    print("client connected")
+    print(f"Client id:{request.sid} is connected")
     print("*************************************")
     emit("connected", f"id:{request.sid} is connected")
 
@@ -42,56 +26,6 @@ def handle_hello(arg):
     except Exception as e:
         print("Error: ", str(e))
         emit("response", "Error occurred", broadcast=True)
-
-
-@socketio.on("connect_drone")
-def handle_custom_event():
-    print("[REQUEST] turn on drone GPS coordinates")
-
-    try:
-        connect_to_hardware()
-
-        # Send a message to the BG96 modem
-        bg96_socket.send(b"TURN ON GPS COORDINATES\r\n")
-
-        # Receive a response from the BG96 modem
-        response = bg96_socket.recv(1024)
-
-        # Close the socket connection
-        # TODO: We probably want to keep this socket open for continuing broadcast
-        # bg96_socket.close()
-
-        # Emit the response to the client
-        emit("GPS", response.decode("utf-8"), broadcast=True)
-
-    except Exception as e:
-        print("Error: ", str(e))
-        emit("GPS", "Error occurred connecting to drone", broadcast=True)
-
-
-@socketio.on("request_sensor")
-def handle_custom_event():
-    print("[REQUEST] turn on drone GPS coordinates")
-
-    try:
-        connect_to_hardware()
-
-        # Send a message to the BG96 modem
-        bg96_socket.send(b"TURN ON SENSOR READINGS\r\n")
-
-        # Receive a response from the BG96 modem
-        response = bg96_socket.recv(1024)
-
-        # Close the socket connection
-        # TODO: We probably want to keep this socket open for continuing broadcast
-        # bg96_socket.close()
-
-        # Emit the response to the client
-        emit("sensor", response.decode("utf-8"), broadcast=True)
-
-    except Exception as e:
-        print("Error: ", str(e))
-        emit("sensor", "Error occurred requesting sensor readings", broadcast=True)
 
 
 @socketio.on("disconnect")
